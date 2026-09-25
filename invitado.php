7ca6b770ce7859dfd8f50728aa5739564df6bc26
Kaledsreviil <luiskaleds@gmail.com>
2026-09-25 09:35:55 -0600

    Agrego cambios de invitado

--- invitado.php
+++ invitado.php
@@ -1,8 +1,10 @@
 <?php include 'public/encabezado.php'; ?>
 
-<div class="card">
-    <h1>Sección de Invitados</h1>
-    <p>Bienvenido, estás navegando como invitado.</p>
-</div>
+<main>
+    <h1>Invitado 1</h1>
+    <img src="loco.jpeg" class="rounded mx-auto d-block" alt="...">
+    <p>Luis Kaled Santos</p>
+    <p> Nacido el 31 de Marzo de 2005, estudiante de Sistemas computacionales en TecNM San Martin Texmelucan </p>
+</main>
 
 <?php include 'public/pie.php'; ?>
